4aa799b1ad088c77c41dbed0f2b671cc61de5fb2
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-11-02 15:20:43 -0600

    resolve conflict for merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import SoupView from './components/SoupView';
 import './App.css';
+=======
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Container from './components/Container';
+
+import './App.css';
+import LinkContainer from './components/LinkContainer';
+>>>>>>> Stashed changes
 
 
 const App: React.FC = () => {
 
   return (
+<<<<<<< Updated upstream
   <main className='main-container'>
     <h1>SOUPWARTS</h1>
     <p>Pick your house and receive your soup recipe!</p>
     <Route exact path="/" render={() => <Home />} />
     <Route path="/" render={() => <SoupView />} />
   </main>);
+=======
+  <div>
+    <h1>Soupwarts</h1>
+    <Route path="/" render={() => <LinkContainer />}/>
+    <Route path="/" render={() => <Container />}/>
+    
+  </div>);
+>>>>>>> Stashed changes
 };
 
 export default App;
---
+++ src/components/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import SoupCard from './SoupCard'
+
+
+export default function Container() {
+  return (
+    <div>
+    <Route exact path='/hufflepuff' render={() => <SoupCard house='egg soup'/>}/>
+     <Route exact path='/ravenclaw' render={() => <SoupCard house='veggie soup'/>}/>
+     <Route exact path='/gryffindor' render={() => <SoupCard house='stew'/>}/>
+     <Route exact path='/slytherin' render={() => <SoupCard house='broth'/>}/>
+    </div>
+  )
+}
