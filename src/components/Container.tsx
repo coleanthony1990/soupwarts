640a6ddd5127a93128b80f33db466e76eb90dd81
Hannah.Celemen <hcelemen@icloud.com>
2022-11-01 22:04:18 -0600

    add changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,23 +1,62 @@
-<<<<<<< HEAD
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Link, Route, Switch, useHistory } from 'react-router-dom';
+import ravenclaw from '../assets/ravenclaw.jpeg';
+import gryffindor from '../assets/gryffindor.jpeg';
+import hufflepuff from '../assets/hufflepuff.jpeg';
+import slytherin from '../assets/slytherin.jpeg';
+import SoupCard from './SoupCard';
 
 export default function Container() {
-  return <div>Container</div>;
-=======
-import React from 'react'
-import { Link, Route, Switch } from 'react-router-dom';
-import SoupCard from './SoupCard'
+  let history = useHistory();
 
-export default function Container() {
   return (
     <div>
-      
-     <Route exact path='/hufflepuff' render={() => <SoupCard house='egg soup'/>}/>
-     <Route exact path='/ravenclaw' render={() => <SoupCard house='veggie soup'/>}/>
-     <Route exact path='/gryffindor' render={() => <SoupCard house='stew'/>}/>
-     <Route exact path='/slytherin' render={() => <SoupCard house='broth'/>}/>
+      <button>
+        <img src={hufflepuff}></img>
+      </button>
+      <button>
+        <img src={ravenclaw}></img>
+      </button>
+      <button>
+        <img src={gryffindor}></img>
+      </button>
+      <button>
+        <img src={slytherin}></img>
+      </button>
+      {/*   <Switch>
+    //     <Route
+    //       exact
+    //       path="/hufflepuff"
+    //       render={() => <SoupCard house="egg soup" />}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/ravenclaw"
+    //       render={() => <SoupCard house="veggie soup" />}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/gryffindor"
+    //       render={() => <SoupCard house="stew" />}
+    //     />
+    //     <Route
+    //       exact
+    //       path="/slytherin"
+    //       render={() => <SoupCard house="broth" />}
+    //     />
+    //   </Switch>
+      {/* <Link to="/hufflepuff">
+        <img src={hufflepuff}></img>
+      </Link>
+      <Link to="/ravenclaw">
+        <img src={ravenclaw}></img>
+      </Link>
+      <Link to="/gryffindor">
+        <img src={gryffindor}></img>
+      </Link>
+      <Link to="/slytherin">
+        <img src={slytherin}></img>
+      </Link> */}
     </div>
-  )
->>>>>>> 4c426864126a064aa1d49e5fd84394818a85cafb
+  );
 }
