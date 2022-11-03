77cac892c03181cb483bf0545fa0047597ab3f67
Hannah.Celemen <hcelemen@icloud.com>
2022-11-02 20:10:52 -0600

    Add error handling and breakpoints to soup card

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import Home from './components/Home';
 import SoupView from './components/SoupView';
 import './App.css';
 
-
 const App: React.FC = () => {
-
   return (
-  <main className='main-container'>
-    <h1>SOUPWARTS</h1>
-    <Route exact path="/" render={() => <Home />} />
-    <Route path="/" render={() => <SoupView />} />
-  </main>);
+    <main className="main-container">
+      <h1 className="header">SOUPWARTS</h1>
+      <Route exact path="/" render={() => <Home />} />
+      <Route path="/" render={() => <SoupView />} />
+    </main>
+  );
 };
 
 export default App;
