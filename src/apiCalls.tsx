8380712bd6faaa01344e2e5bb547dd44f5dca273
Hannah.Celemen <hcelemen@icloud.com>
2022-11-07 16:12:02 -0700

    Refactor getSoups function to clean data before set state

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,19 +1,5 @@
 import { Recipe } from './components/SoupCard';
 
-// export const getSoups = async (house: string): Promise<Recipe[]> => {
-//   const options: RequestInit = {
-//     method: 'GET',
-//     headers: {
-//       'X-Api-Key': 'GDaJLJYwIJFvDvWHi24M9A==YA7y1zm22w9L6WhF',
-//     },
-//   };
-//   const response = await fetch(
-//     `https://api.api-ninjas.com/v1/recipe?query=${house}`,
-//     options
-//   );
-//   return await response.json();
-// };
-
 export const getSoups = async (house: string): Promise<Recipe[]> => {
   const options: RequestInit = {
     method: 'GET',
