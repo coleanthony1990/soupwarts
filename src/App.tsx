46e757dab57abf3d9e70221e8b1942fc99f740e1
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-11-06 18:40:53 -0700

    import quotes and randomize

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import SoupView from './components/SoupView';
 import './App.css';
+import quotes from './assets/quotes.json'
 
 const App: React.FC = () => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
   return (
     <main className="main-container">
       <h1 className="header">SOUPWARTS</h1>
-      <Route exact path="/" render={() => <Home />} />
+      <Route exact path="/" render={() => <Home quote={randomQuote}/>} />
       <Route path="/" render={() => <SoupView />} />
     </main>
   );
